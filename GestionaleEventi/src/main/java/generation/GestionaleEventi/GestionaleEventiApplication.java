3e57c527226f5fafd4cdd3ea631f0e4216e63b2b
crd-luca <161834926+crd-luca@users.noreply.github.com>
2024-07-30 17:07:59 +0200

    Add files via upload

---
+++ GestionaleEventi/src/main/java/generation/GestionaleEventi/GestionaleEventiApplication.java
@@ -0,0 +1,13 @@
+package generation.GestionaleEventi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionaleEventiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionaleEventiApplication.class, args);
+	}
+
+}
