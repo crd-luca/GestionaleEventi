d0b44d9068a166686992e19fe9b4a81cb57a7177
Alessandro <alexratti29@gmail.com>
2024-08-02 10:10:41 +0200

    repository

---
+++ src/main/java/generation/GestionaleEventi/repositories/GestoreRepository.java
@@ -0,0 +1,8 @@
+package generation.GestionaleEventi.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GestoreRepository extends JpaRepository<Gestore, Long>
+{
+    
+}
---
+++ src/main/java/generation/GestionaleEventi/services/AppService.java
@@ -0,0 +1,8 @@
+package main.java.generation.GestionaleEventi.services;
+import org.springframework.stereotype.Service;
+
+@Service
+@Data
+public class AppService {
+    private String message;
+}
