14ace3fc6ec89fe3c5dda31a7c859ccaf8ecf710
Alessandro <alexratti29@gmail.com>
2024-08-02 10:15:50 +0200

    ZIOPORCO

---
+++ src/main/java/generation/GestionaleEventi/repositories/EventoRepository.java
@@ -0,0 +1,9 @@
+package generation.GestionaleEventi.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventoRepository extends JpaRepository<Evento,Long>
+ 
+{
+    
+}
